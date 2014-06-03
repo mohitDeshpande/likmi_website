989e7e0f5d24da4cd704f93ab708c9bc259b8e56
Mohit Deshpande <mohitdeshpande@icloud.com>
2014-06-03 22:27:46 +0530

    Initial Commit
    initial commit just after creating the app using rails new command,
    complete bare bones app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LikmiWebsite::Application.config.secret_key_base = 'd705fc67d2d395dcd725036d50ea69ea92bd58b120679bb12e7cd0a3f706fcf4a18c8f830fcd48cad10a6c6c9ec73b45971edd2e52604a15b7db7099311dec74'
