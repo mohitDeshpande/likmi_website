4e1b4e356d59e828f421ce6487da07d488008980
Mohit Deshpande <mohitdeshpande@icloud.com>
2014-06-08 19:02:58 +0530

    created home and menu pages
    home and menu pages
    along with providing title in the layout.
    also added a dynamic secure token rather than a static one for safety

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-LikmiWebsite::Application.config.secret_key_base = 'd705fc67d2d395dcd725036d50ea69ea92bd58b120679bb12e7cd0a3f706fcf4a18c8f830fcd48cad10a6c6c9ec73b45971edd2e52604a15b7db7099311dec74'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end		
+
+LikmiWebsite::Application.config.secret_key_base = secure_token
